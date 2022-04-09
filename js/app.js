1096967ebc591351f08bb1c5b0b1ea245ec87487
Mona Lisa <santifranquez@gmail.com>
2022-04-08 22:24:11 -0300

    mejora en el codigo del carrito

--- js/app.js
+++ js/app.js
@@ -1,124 +1,116 @@
-// VARIABLES
-const espacioCarrito = document.querySelector('#carrito');
-const vaciarCarrito = document.querySelector('#vaciar-carrito')
+// Variables
 const tbody = document.querySelector('tbody');
+const vaciarCarrito = document.querySelector('#vaciar-carrito');
 const listaCursos = document.querySelector('#lista-cursos');
+const carrito = document.querySelector('#carrito')
 
-let arregloCarrito = [];
+let arregloCarrito = []
 
-// Funciones de click
-listaCursos.addEventListener('click', agregarCarroBtn)
+// Eventos
+listaCursos.addEventListener('click', agregarCursos);
 vaciarCarrito.addEventListener('click', borrarTodo)
-espacioCarrito.addEventListener('click', borrarCurso)
+carrito.addEventListener('click', borrarUnCurso)
 
 
-function agregarCarroBtn(e) {
-    e.preventDefault()
-
+// funciones
+function agregarCursos(e) {
+    e.preventDefault();
     // console.log(e.target)
+
     if(e.target.classList.contains('agregar-carrito')) {
-        // console.log(e.target.parentElement.parentElement)
         const card = e.target.parentElement.parentElement;
         // console.log(card)
 
-        const infoCursos = {
-            imagen: card.querySelector('img').src,
+        informacionCursos = {
             titulo: card.querySelector('h4').textContent,
+            imagen: card.querySelector('img').src,
             precio: card.querySelector('.precio span').textContent,
-            cantidad: 1,
-            id: card.querySelector('a').getAttribute('data-id')
+            id: card.querySelector('a').getAttribute('data-id'),
+            cantidad: 1
         }
 
-        // console.log(infoCursos)
-
-        // Que no se repita un curso si ya está siendo puesto
-        const existe = arregloCarrito.some((curso) => {
-            return curso.id === infoCursos.id
+        const existe = arregloCarrito.some((curso) =>{
+            return curso.id === informacionCursos.id
         })
 
         if(existe) {
-            // Acà actuara si la cantidad es mayor a uno
             const cursos = arregloCarrito.map((curso) => {
-                if(curso.id === infoCursos.id) {
+                if(curso.id === informacionCursos.id) {
                     curso.cantidad++
                     return curso
                 } else {
                     return curso
                 }
-
             })
-
-            arregloCarrito = [...cursos]
-
         } else {
-            // Si el curso no se repite actuara normalmente
-            arregloCarrito = [...arregloCarrito, infoCursos]
+            arregloCarrito = [...arregloCarrito, informacionCursos]
         }
-        
-        
-        
-        console.log(arregloCarrito)
+
+
 
         mostrarHTML();
     }
-
 }
 
 function mostrarHTML() {
 
-    limpiarHTML();
+    // Se limpia el html para que no se repitan los cursos en cada arreglo
+    limpiarHTML()
 
     arregloCarrito.forEach((curso) => {
-
         const fila = document.createElement('tr');
 
-        fila.innerHTML = `
+        fila.innerHTML= `
             <td>
-                <img src="${curso.imagen}" width=150>
+                <img src="${curso.imagen}" width=120>
             </td>
+
             <td>
                 ${curso.titulo}
             </td>
+
             <td>
                 ${curso.precio}
             </td>
+
             <td>
                 ${curso.cantidad}
             </td>
+
             <td>
-                <a href="#" class="borrar-curso" data-id="${curso.id}">X</a>
+            <a href="#" data-id="${curso.id}" class="borrar-curso">X</a>
             </td>
         `;
 
         tbody.appendChild(fila)
-
-
     })
+
 }
 
-function limpiarHTML() {
-    tbody.innerHTML= ''
-}
+// Se usa esta forma para eliminar un curso en particular
+function borrarUnCurso(e) {
 
-// Funcion para borrar todos los cursos disponibles
-function borrarTodo() {
-    arregloCarrito = []; // El arreglo se pone vacio
-    console.log(arregloCarrito)
-
-    limpiarHTML(); // y se llama a la función de limpiar html para que todo se exprese en el html
-}
-
-function borrarCurso(e) {
-    // console.log(e.target)
+    // evitamos el evnet bubbling seleccionando el boton de la clase
     if(e.target.classList.contains('borrar-curso')) {
-        // Para eliminar un curso usaremos la propiedad de .filter
-
+        // Accedemos a su id
         const cursoId = e.target.getAttribute('data-id')
 
+        // y usamos .filter
         arregloCarrito = arregloCarrito.filter((curso) => {
             return curso.id !== cursoId
         })
-    }
 
-    mostrarHTML();
+        mostrarHTML()
+    }
+}
+
+
+function limpiarHTML() {
+    tbody.innerHTML = ''
+}
+
+function borrarTodo() {
+    arregloCarrito = [];
+
+    limpiarHTML();
 }
