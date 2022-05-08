0e93e4a29d04dce3b05e7ead7d84c545043cb2ab
Mona Lisa <santifranquez@gmail.com>
2022-05-08 14:40:22 -0300

    agregado el modo oscuro, y toda la funcionalidad

--- js/app.js
+++ js/app.js
@@ -5,12 +5,17 @@ const listaCursos = document.querySelector('#lista-cursos');
 const carrito = document.querySelector('#carrito')
 const card = document.querySelector('.agregar-carrito');
 
+
 let arregloCarrito = []
 
 document.addEventListener('DOMContentLoaded', () => {
     listaCursos.addEventListener('click', buscarCurso);
 
-    vaciarCarrito.addEventListener('click', borrarCursos)
+    vaciarCarrito.addEventListener('click', borrarCursos);
+
+    carrito.addEventListener('click', eliminarCurso);
+
+    mantenerloEnHTML()
 })
 
 
@@ -37,9 +42,36 @@ function leerCurso(e) {
         cantidad: 1
     }
 
-    arregloCarrito = [...arregloCarrito, OBJCurso];
+    
 
-    console.log(arregloCarrito)
+    // console.log(arregloCarrito)
+
+    const existe = arregloCarrito.some((curso) => {
+        return curso.id === OBJCurso.id
+    })
+
+    if(existe) {
+        const cursos = arregloCarrito.map((curso) => {
+            if(curso.id === OBJCurso.id) {
+                curso.cantidad++
+                return curso // retorna el curso individual
+            } else {
+                return curso
+            }
+
+        });
+
+        arregloCarrito =[...cursos]
+    } else {
+        // Si el curso no está repetido entonces se ejecuta esto
+        arregloCarrito = [...arregloCarrito, OBJCurso];
+    }
+
+   
+
+    // console.
+
+    console.log(existe)
 
     agregarHTML();
 }
@@ -57,10 +89,14 @@ function agregarHTML() {
             <td>${curso.nombre}</td>
             <td>${curso.precio}</td>
             <td>${curso.cantidad}</td>
+            <td><a href="#" class="borrar-curso" data-id="${curso.id}">X</td>
         `;
 
         tbody.appendChild(tr)
     })
+
+    //Llamado al localStorage
+    sincronizarStorage();
 }
 
 
@@ -88,3 +124,56 @@ function borrarCursos() {
     
 
 }
+
+function eliminarCurso(e) {
+    if(e.target.classList.contains('borrar-curso')) {
+        // Obtenemos el id del curso que clickeamos.
+        const idDelCurso = e.target.getAttribute('data-id');
+        
+        // en el arreglo, lo abrimos y filtramos aquellos que no conicidan con la id del curso
+        // basicamente lo que hace en terminos simples es traerse todos los cursos, excepto el que nosotros le dimos click
+        arregloCarrito = arregloCarrito.filter((curso) => {
+            return curso.id !== idDelCurso
+        })
+
+        agregarHTML()
+    }
+}
+
+//LocalStorage sección
+function sincronizarStorage() {
+    //Añadir al storage
+
+    //Pasarlo a texto
+    const arreglocarritoTexto = JSON.stringify(arregloCarrito);
+    localStorage.setItem('productos', arreglocarritoTexto);
+}
+
+// Asi se mantiene en el html
+// se consigue el localstorage , y se pone los [] en caso de que el elemento este vacio y no se agrego nada
+function mantenerloEnHTML() {
+
+    const prueba = JSON.parse(localStorage.getItem('productos'));
+    // JSON.parse(prueba)
+    arregloCarrito = prueba || [];
+
+    agregarHTML()
+}
+
+
+// Modo oscuro
+
+const boton = document.querySelector('#img-carrito-2');
+const body = document.querySelector('body')
+const header = document.querySelector('.header')
+const carta = document.querySelector('.card')
+
+console.log(carta)
+boton.addEventListener('click', () => {
+    
+    
+    body.classList.toggle('modooscuro')
+    header.classList.toggle('modooscuro')
+    carta.classList.toggle('card-oscuro')
+    carrito.style.color = 'black'
+})
